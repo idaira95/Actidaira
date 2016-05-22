116a8b63cc4ac4733ae5098a05838184f10f6cda
Idaira Isabel <Idaira Isabel>
2016-05-22 19:35:27 +0200

    sexto commit clase banquero

---
+++ Banquerogit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banquerogit;
+
+/**
+ *
+ * @author SATELLITE C55A
+ */
+public class Banquerogit {
+
+        private String nombre;
+	private String descripcion;
+	
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
